ce12b9288345eda9bc3df0c58930a8978ccdecab
Sebastian <sebastiano139@gmail.com>
2021-03-22 19:18:19 +0100

    [FIX-1] Fixed npm run build command

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
