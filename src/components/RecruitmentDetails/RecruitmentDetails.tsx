465ed666754495f0179e35eeecaf5409352fc2ce
Konrad Bochnia <kbochnia@gmail.com>
2021-05-09 20:36:24 +0200

    CS-68 Add pagination to recruitment details page (#20)
    * CS-49 Add ability to sort columns
    
    * CS-68 Add pagination to recruitment details page
    
    Co-authored-by: Konrad Bochnia <kbochnia@virtuslab.com>

--- src/components/RecruitmentDetails/RecruitmentDetails.tsx
+++ src/components/RecruitmentDetails/RecruitmentDetails.tsx
@@ -16,30 +16,232 @@ import {
     ModalContent,
     ModalHeader,
     ModalFooter,
+    chakra,
+    NumberInput,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInputField,
+    NumberInputStepper,
+    Select,
+    HStack,
 } from '@chakra-ui/react';
 import NavBar from '../utils/NavBar';
 import { useState, useEffect } from 'react';
 import {
     getRecruitmentDetails,
-    RecruitmentDetails,
+    RecruitmentDetails as RecruitmentDetailsType,
+    Candidate,
 } from './../../services/api';
-import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
+import {
+    CheckIcon,
+    CloseIcon,
+    TriangleDownIcon,
+    TriangleUpIcon,
+} from '@chakra-ui/icons';
 import RecruitmentSummary from '../RecruitmentSummary/RecruitmentSummary';
+import { Column, usePagination, useSortBy, useTable } from 'react-table';
+
+interface DetailsTableProps {
+    details: RecruitmentDetailsType;
+}
+
+function DetailsTable({ details }: DetailsTableProps) {
+    const isSecondDegree = details.degree === 'Studia drugiego stopnia';
+
+    const paidIcon = <CheckIcon color="green" />;
+    const unpaidIcon = <CloseIcon color="red" />;
+
+    const data = React.useMemo(() => details.candidates, [details]);
+    const columns = React.useMemo<Column<Candidate>[]>(
+        () => [
+            {
+                Header: 'L.P.',
+                Cell: (row: { row: { id: number } }) => {
+                    return <div>{+row.row.id + 1}</div>;
+                },
+                disableSortBy: true,
+            },
+            { Header: 'Imię i nazwisko', accessor: 'name' },
+            { Header: 'Kraj', accessor: 'country' },
+            { Header: 'Województwo', accessor: 'region' },
+            { Header: 'Miasto', accessor: 'city' },
+            ...((isSecondDegree
+                ? [
+                      { Header: 'Uczelnia', accessor: 'college_name' },
+                      { Header: 'Kierunek', accessor: 'field_of_study' },
+                      { Header: 'Wynik testu', accessor: 'test_points' },
+                  ]
+                : [
+                      {
+                          Header: 'Liceum',
+                          accessor: (row) =>
+                              row.highschool + ';' + row.highschool_city,
+                      },
+                      { Header: 'Wynik matury', accessor: 'matura_points' },
+                  ]) as Column<Candidate>[]),
+            { Header: 'Liczba punktów', accessor: 'points' },
+            { Header: 'Pesel', accessor: 'pesel' },
+            {
+                Header: 'Opłata rekrutacyjna',
+                accessor: (row) => (row.is_paid ? paidIcon : unpaidIcon),
+                id: 'is_paid',
+                sortType: (a, b, id) =>
+                    (a.original as any)[id]
+                        ? 1
+                        : (b.original as any)[id]
+                        ? -1
+                        : 0,
+            },
+        ],
+        [details],
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable({ data, columns }, useSortBy, usePagination);
+
+    console.log(pageCount);
+
+    return (
+        <>
+            <Table
+                colorScheme="blue"
+                mt="5"
+                textAlign="center"
+                {...getTableProps()}
+            >
+                <Thead>
+                    {headerGroups.map((group) => (
+                        <Tr fontWeight="600" {...group.getHeaderGroupProps()}>
+                            {group.headers.map((col) => (
+                                <Th
+                                    {...col.getHeaderProps(
+                                        col.getSortByToggleProps(),
+                                    )}
+                                >
+                                    {col.render('Header')}
+                                    <chakra.span pl="2">
+                                        {col.isSorted ? (
+                                            col.isSortedDesc ? (
+                                                <TriangleDownIcon />
+                                            ) : (
+                                                <TriangleUpIcon />
+                                            )
+                                        ) : null}
+                                    </chakra.span>
+                                </Th>
+                            ))}
+                        </Tr>
+                    ))}
+                </Thead>
+                <Tbody {...getTableBodyProps()}>
+                    {page.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <Tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => (
+                                    <Td {...cell.getCellProps()}>
+                                        {cell.render('Cell')}
+                                    </Td>
+                                ))}
+                            </Tr>
+                        );
+                    })}
+                </Tbody>
+            </Table>
+            <Flex
+                bg="white"
+                p="3"
+                alignItems="center"
+                mt="2"
+                mb="3"
+                borderRadius="xl"
+            >
+                <HStack spacing="1">
+                    <Button
+                        colorScheme="blue"
+                        onClick={() => gotoPage(0)}
+                        disabled={!canPreviousPage}
+                    >
+                        {'<<'}
+                    </Button>
+                    <Button
+                        colorScheme="blue"
+                        onClick={() => previousPage()}
+                        disabled={!canPreviousPage}
+                    >
+                        {'<'}
+                    </Button>
+                    <Button
+                        colorScheme="blue"
+                        onClick={() => nextPage()}
+                        disabled={!canNextPage}
+                    >
+                        {'>'}
+                    </Button>
+                    <Button
+                        colorScheme="blue"
+                        onClick={() => gotoPage(pageCount - 1)}
+                        disabled={!canNextPage}
+                    >
+                        {'>>'}
+                    </Button>
+                </HStack>
+                <Text pl="2">
+                    Strona {pageIndex + 1} z {pageCount} | Przejdź do strony
+                </Text>
+                <NumberInput
+                    defaultValue={1}
+                    min={1}
+                    max={pageCount}
+                    onChange={(p) => gotoPage(+p - 1)}
+                    width="20"
+                    pl="1"
+                >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+                <Select
+                    value={pageSize}
+                    onChange={(e) => setPageSize(+e.target.value)}
+                    width="30"
+                    pl="2"
+                >
+                    {[10, 20, 30, 40, 50].map((size) => (
+                        <option value={size}>Pokaż {size}</option>
+                    ))}
+                </Select>
+            </Flex>
+        </>
+    );
+}
 
 interface Props {
     id: number;
 }
 
 export default function RecruitmentDetails({ id }: Props) {
-    const [details, setDetails] = useState<RecruitmentDetails>();
+    const [details, setDetails] = useState<RecruitmentDetailsType>();
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     useEffect(() => {
         getRecruitmentDetails(id).then(setDetails);
     }, [id]);
-
-    const isSecondDegree =
-        (details && details.degree) === 'Studia drugiego stopnia';
 
     const paidIcon = <CheckIcon color="green" />;
     const unpaidIcon = <CloseIcon color="red" />;
@@ -101,85 +303,7 @@ export default function RecruitmentDetails({ id }: Props) {
                 ml="5"
                 mr="5"
             >
-                <Table colorScheme="blue" mt="5" textAlign="center">
-                    <Thead>
-                        <Tr fontWeight="600">
-                            <Th>L.P.</Th>
-                            <Th>Imię i nazwisko</Th>
-                            <Th>Kraj</Th>
-                            <Th>Województwo</Th>
-                            <Th>Miasto</Th>
-                            {isSecondDegree ? (
-                                <>
-                                    <Th>Uczelnia</Th>
-                                    <Th>Kierunek</Th>
-                                    <Th>Wynik testu</Th>
-                                </>
-                            ) : (
-                                <>
-                                    <Th>Liceum</Th>
-                                    <Th>Wynik matury</Th>
-                                </>
-                            )}
-                            <Th>Liczba punktów</Th>
-                            <Th>Pesel</Th>
-                            <Th>Opłata rekrutacyjna</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {details &&
-                            details.candidates.map(
-                                (
-                                    {
-                                        id,
-                                        name,
-                                        country,
-                                        city,
-                                        region,
-                                        college_name,
-                                        highschool,
-                                        highschool_city,
-                                        field_of_study,
-                                        matura_points,
-                                        test_points,
-                                        points,
-                                        pesel,
-                                        is_paid,
-                                    },
-                                    i,
-                                ) => (
-                                    <Tr key={id}>
-                                        <Td>{i + 1}</Td>
-                                        <Td>{name}</Td>
-                                        <Td>{country}</Td>
-                                        <Td>{region}</Td>
-                                        <Td>{city}</Td>
-                                        {isSecondDegree ? (
-                                            <>
-                                                <Td>{college_name}</Td>
-                                                <Td>{field_of_study}</Td>
-                                                <Td>{test_points}</Td>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <Td>
-                                                    {highschool};{' '}
-                                                    {highschool_city}
-                                                </Td>
-                                                <Td>{matura_points}</Td>
-                                            </>
-                                        )}
-                                        <Td>{points}</Td>
-                                        <Td>{pesel}</Td>
-                                        <Td>
-                                            {is_paid && paidIcon}
-                                            {!is_paid && unpaidIcon}
-                                        </Td>
-                                    </Tr>
-                                ),
-                            )}
-                    </Tbody>
-                </Table>
+                {details && <DetailsTable details={details} />}
             </Flex>
         </Flex>
     );
