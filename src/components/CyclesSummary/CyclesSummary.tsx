6f8e2d39cb8cd6758a3a1ada97fa746848f0878d
Sebastian <sebastiano139@gmail.com>
2021-05-28 09:44:52 +0200

    CS-89 Fixed snowpack problem

--- src/components/CyclesSummary/CyclesSummary.tsx
+++ src/components/CyclesSummary/CyclesSummary.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ReactElement } from 'react';
 
 import { Flex } from '@chakra-ui/layout';
-import type { ReactElement } from 'react';
 import { getCyclesSummary, CyclesSummary } from '../../services/api';
 import { Text } from '@chakra-ui/react';
 interface CyclesSummaryProps {
@@ -11,7 +10,9 @@ interface CyclesSummaryProps {
 export default function CyclesSummary({
     id,
 }: CyclesSummaryProps): ReactElement {
-    const [cyclesSummary, setCyclesSummary] = useState<CyclesSummary>();
+    const [cyclesSummary, setCyclesSummary] = useState<CyclesSummary>(
+        {} as CyclesSummary,
+    );
 
     useEffect(() => {
         getCyclesSummary(id).then(setCyclesSummary);
@@ -21,21 +22,21 @@ export default function CyclesSummary({
         <Flex direction="column">
             <Text>
                 <b>Ilość cykli: </b>
-                {cyclesSummary?.cycles_number}
+                {cyclesSummary.cycles_number}
             </Text>
             <Text>
                 <b>Status: </b>
-                <Text as="b" color={cyclesSummary?.is_active ? 'green' : 'red'}>
-                    {cyclesSummary?.is_active ? 'AKTYWNA' : 'NIEAKTYWNA'}{' '}
+                <Text as="b" color={cyclesSummary.is_active ? 'green' : 'red'}>
+                    {cyclesSummary.is_active ? 'AKTYWNA' : 'NIEAKTYWNA'}{' '}
                 </Text>
             </Text>
             <Text>
                 <b>Ilość wszystkich kandydatów: </b>
-                {cyclesSummary?.overall_candidates_num}
+                {cyclesSummary.overall_candidates_num}
             </Text>
             <Text>
                 <b>Ilość zakwalifikowanych kandydatów: </b>
-                {cyclesSummary?.overall_qualified}
+                {cyclesSummary.overall_qualified}
             </Text>
         </Flex>
     );
